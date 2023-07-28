93d0c2b83d955053a9d7d20e92edaa549a487685
Jonathan Green <jonathan@razzard.com>
2023-07-28 17:17:40 -0300

    Cleanup

--- download_opds.py
+++ download_opds.py
@@ -1,13 +1,41 @@
+from __future__ import annotations
+
 import argparse
 import json
 import math
 import sys
 from pathlib import Path
-from typing import Dict, Any
+from typing import Any, Dict
 
 import requests
 from alive_progress import alive_bar
 from requests import Session
+
+
+def flatten_dict(
+    dictionary: Dict[str, Any], parent_key: Otional[str] = None, separator: str = "."
+) -> Dict[str, str]:
+    items = []
+    for key, value in dictionary.items():
+        new_key = str(parent_key) + separator + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            if not value.items():
+                items.append((new_key, None))
+            else:
+                items.extend(flatten_dict(value, new_key, separator).items())
+        elif isinstance(value, list):
+            if len(value):
+                for k, v in enumerate(value):
+                    items.extend(flatten_dict({str(k): v}, new_key, separator).items())
+            else:
+                items.append((new_key, None))
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+def flatten_list(data: List[Dict[str, Any]]) -> List[Dict[str, str]]:
+    return [flatten_dict(d) for d in data]
 
 
 def make_request(session: Session, url: str) -> Dict[str, Any]:
@@ -19,17 +47,28 @@ def make_request(session: Session, url: str) -> Dict[str, Any]:
     return response.json()
 
 
-if __name__ == '__main__':
+def write_json(file: File, data: List[Dict[str, Any]]) -> None:
+    file.write(json.dumps(data, indent=4))
+
+
+def write_csv(file: File, data: List[Dict[str, Any]]) -> None:
+    flattened = flatten_list(data)
+    df = pd.json_normalize(flattened)
+    df.to_csv(file, index=False, encoding="utf-8")
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    prog='download_opds',
-                    description='Download OPDS 2 feed')
+        prog="download_opds", description="Download OPDS 2 feed"
+    )
 
-    parser.add_argument('url', help='URL of feed')
-    parser.add_argument('-u', '--username', help='Username')
-    parser.add_argument('-p', '--password', help='Password')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument("url", help="URL of feed")
+    parser.add_argument("output_file", help="Output file")
+    parser.add_argument("-u", "--username", help="Username")
+    parser.add_argument("-p", "--password", help="Password")
+    parser.add_argument("-c", "--csv", help="Output CSV file", action="store_true")
 
-    # Get args from comand line
+    # Get args from command line
     args = parser.parse_args()
 
     # Create a session to fetch the documents
@@ -38,7 +77,7 @@ if __name__ == '__main__':
     if args.username and args.password:
         session.auth = (args.username, args.password)
 
-    session.headers.update({'Accept': 'application/opds+json'})
+    session.headers.update({"Accept": "application/opds+json"})
 
     publications = []
 
@@ -46,7 +85,7 @@ if __name__ == '__main__':
     response = make_request(session, args.url)
     items = response["metadata"]["numberOfItems"]
     items_per_page = response["metadata"]["itemsPerPage"]
-    pages = math.ceil(items/items_per_page)
+    pages = math.ceil(items / items_per_page)
 
     # Fetch the rest of the pages:
     next_url = args.url
@@ -61,8 +100,8 @@ if __name__ == '__main__':
                     break
             bar()
 
-    if args.output:
-        with Path(args.output).open('w') as file:
-            file.write(json.dumps(publications, indent=4))
-    else:
-        print(json.dumps(publications, indent=4))
+    with Path(args.output_file).open("w") as file:
+        if args.csv:
+            write_csv(file, publications)
+        else:
+            write_json(file, publications)
--- opds_to_csv.py
+++ opds_to_csv.py
@@ -1,18 +1,19 @@
-import pandas as pd
-from pathlib import Path
 import json
-import csv
+from collections.abc import MutableMapping
+from pathlib import Path
 
-from collections import MutableMapping
+import pandas as pd
 
 
-def flatten(dictionary: Dict[str, Any], parent_key: Otional[str] = None, separator: str = '.'):
+def flatten(
+    dictionary: Dict[str, Any], parent_key: Otional[str] = None, separator: str = "."
+):
     items = []
     for key, value in dictionary.items():
         new_key = str(parent_key) + separator + key if parent_key else key
         if isinstance(value, MutableMapping):
             if not value.items():
-                items.append((new_key,None))
+                items.append((new_key, None))
             else:
                 items.extend(flatten(value, new_key, separator).items())
         elif isinstance(value, list):
@@ -20,7 +21,7 @@ def flatten(dictionary: Dict[str, Any], parent_key: Otional[str] = None, separat
                 for k, v in enumerate(value):
                     items.extend(flatten({str(k): v}, new_key, separator).items())
             else:
-                items.append((new_key,None))
+                items.append((new_key, None))
         else:
             items.append((new_key, value))
     return dict(items)
@@ -30,7 +31,7 @@ def flatten(dictionary: Dict[str, Any], parent_key: Otional[str] = None, separat
 p = Path("combined.json")
 
 # Open file
-with p.open('r', encoding='utf-8') as f:
+with p.open("r", encoding="utf-8") as f:
     data = json.loads(f.read())
 
 flattened = [flatten(d) for d in data]
@@ -39,4 +40,4 @@ flattened = [flatten(d) for d in data]
 df = pd.json_normalize(flattened)
 
 # save to csv
-df.to_csv('test.csv', index=False, encoding='utf-8')
+df.to_csv("test.csv", index=False, encoding="utf-8")
