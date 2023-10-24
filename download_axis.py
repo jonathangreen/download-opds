440a3876bb833cb63d369fd99a7b7a8fa3c46d97
Jonathan Green <jonathan@razzard.com>
2023-10-24 17:13:37 -0300

    Add ability to query QA endpoint for axis.

--- download_axis.py
+++ download_axis.py
@@ -9,17 +9,20 @@ import requests
 import xmltodict
 
 PRODUCTION_BASE_URL = "https://axis360api.baker-taylor.com/Services/VendorAPI/"
+QA_BASE_URL = "https://axis360apiqa.baker-taylor.com/Services/VendorAPI/"
 
 access_token_endpoint = "accesstoken"
 availability_endpoint = "availability/v2"
 
 
-def get_headers(username: str, password: str, library_id: str) -> Dict[str, str]:
+def get_headers(
+    base_url: str, username: str, password: str, library_id: str
+) -> Dict[str, str]:
     authorization_str = ":".join([username, password, library_id])
     authorization_bytes = authorization_str.encode("utf_16_le")
     authorization_b64 = base64.standard_b64encode(authorization_bytes)
     resp = requests.post(
-        PRODUCTION_BASE_URL + access_token_endpoint,
+        base_url + access_token_endpoint,
         headers={"Authorization": b"Basic " + authorization_b64},
     )
     if resp.status_code != 200:
@@ -33,10 +36,10 @@ def get_headers(username: str, password: str, library_id: str) -> Dict[str, str]
     }
 
 
-def availability(username: str, password: str, library_id: str) -> str:
-    headers = get_headers(username, password, library_id)
+def availability(base_url: str, username: str, password: str, library_id: str) -> str:
+    headers = get_headers(base_url, username, password, library_id)
     resp = requests.get(
-        PRODUCTION_BASE_URL + availability_endpoint,
+        base_url + availability_endpoint,
         headers=headers,
         params={"updatedDate": "1970-01-01 00:00:00"},
     )
@@ -53,12 +56,16 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--password", help="Password", required=True)
     parser.add_argument("-l", "--library-id", help="Library ID", required=True)
     parser.add_argument("-j", "--json", help="Output JSON file", action="store_true")
+    parser.add_argument("-q", "--qa", help="Use QA Endpoint", action="store_true")
 
     # Get args from command line
     args = parser.parse_args()
 
+    # Find the base URL to use
+    base_url = PRODUCTION_BASE_URL if not args.qa else QA_BASE_URL
+
     # Fetch the document as XML
-    xml = availability(args.username, args.password, args.library_id)
+    xml = availability(base_url, args.username, args.password, args.library_id)
 
     if args.json:
         xml_dict = xmltodict.parse(xml)
